f982daab304c431e34402ea2e0e4cdff6071492a
sx-motive <sx.motive@gmail.com>
2023-02-23 02:34:55 +0500

    update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -81,10 +81,48 @@ const toggleMenu = () => {
   header.classList.toggle('with-menu');
 };
 
+const wrap = document.querySelector('[data-img-follow]');
+
+const followImg = (e) => {
+  const viewportWidth = window.innerWidth;
+
+  const mousePos = {
+    X: e.clientX,
+    Y: e.clientY,
+    hor: () => (e.clientX > viewportWidth / 2 ? true : false),
+  };
+
+  wrap.style.transform = `
+  translate(${mousePos.X - 200}px, ${mousePos.Y - 200}px) 
+  rotate(${mousePos.hor() ? '-7deg' : '7deg'})
+  `;
+};
+
 window.onload = () => {
   removeLoader();
   initScroll();
   replaceCaptions();
+
+  const links = document.querySelectorAll('[data-link]');
+  const images = document.querySelectorAll('[data-img]');
+
+  document
+    .getElementsByClassName('essentials')[0]
+    .addEventListener('mousemove', followImg);
+
+  [...links].map((link) => {
+    const num = link.dataset.link;
+    link.addEventListener('mouseenter', () => {
+      [...images].forEach((image) => {
+        image.dataset.img == num ? image.classList.add('active') : '';
+      });
+    });
+    link.addEventListener('mouseleave', () => {
+      [...images].forEach((image) => {
+        image.dataset.img == num ? image.classList.remove('active') : '';
+      });
+    });
+  });
 
   const menuButton = document.getElementsByClassName('menu-btn')[0];
   menuButton.addEventListener('click', toggleMenu);
